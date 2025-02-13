7d6ba2453f1e729f84ce8a4fd83b41ad5198dcad
WilliamNG01 <dawisfr@gmail.com>
2025-02-13 15:42:39 +0100

    Update WeatherForecastController.cs

--- Mardi20250211.A_PL/Controllers/WeatherForecastController.cs
+++ Mardi20250211.A_PL/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Mardi20250211.A_PL.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
